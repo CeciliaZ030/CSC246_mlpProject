3bcc95f12c424c1ad66553a2b370d4e8f627eb79
CeciliaZ030 <CeciliaZ030@users.noreply.github.com>
2020-03-03 18:04:43 -0500

    back prop

--- starterCode/mlp.py
+++ starterCode/mlp.py
@@ -5,7 +5,7 @@ import random
 
 ''' An implementation of an MLP with a single layer of hidden units. '''
 class MLP:
-    __slots__ = ('W1', 'b1', 'a1', 'z1', 'W2', 'b2', 'din', 'dout', 'hidden_units')
+    __slots__ = ('W1', 'b1', 'a1', 'z1', 'W2', 'b2', 'a2', 'din', 'dout', 'hidden_units')
 
     def __init__(self, din, dout, hidden_units):
         ''' Initialize a new MLP with tanh activation and softmax outputs.
@@ -19,16 +19,23 @@ class MLP:
         Biases are initalized to zero.
         
         Note: a1 and z1 can be used for caching during backprop/evaluation.
+              a2 added.
         
         '''
         self.din = din
         self.dout = dout
         self.hidden_units = hidden_units
 
+        self.a1 = None
+        self.z1 = None 
+        self.a2 = None
+
         self.b1 = np.zeros((self.hidden_units, 1))
         self.b2 = np.zeros((self.dout, 1))
-        self.W1 = 2*(np.random.random((self.hidden_units, self.din)) - 0.5)
-        self.W2 = 2*(np.random.random((self.dout, self.hidden_units)) - 0.5)
+        self.W1 = 2*(np.random.random((self.din, self.hidden_units)) - 0.5)
+        self.W2 = 2*(np.random.random((self.hidden_units, self.dout)) - 0.5)
+
+
 
 
     def save(self,filename):
@@ -50,55 +57,58 @@ class MLP:
         '''
         N = xdata.shape[1]
         outputs = np.zeros((N, self.dout))
+
+        self.a1 = np.zeros(N, self.hidden_units)
+        self.z1 = np.zeros(N, self.hidden_units)
+        self.a2 = np.zeros(N, self.dout)
+
         #Go thru data set [0...n-1]
         for n in range(N):
 
             #Hidden layer
-            z = np.zeros(self.hidden_units)
 
             for j in range(self.hidden_units):
                 #adding offset first
-                aj = self.b1[j]
+                self.a1[n][j] = self.b1[j]
                 #Go thru dimension [0...dim-1]
-                for i in range(xdata.shape[0]):
-                    # xni = xdata[n][i] input value, wij = W1[j][i] weight of the input unit towards a hidden unit
-                    aj += xdata[i][n] * W1[j][i]
-                z[j] = np.tanh(aj)
+                for i in range(self.din):
+                    # xdata[n][i] input value, W1[j][i] weight of the input unit towards a hidden unit
+                    self.a1[n][j] += xdata[i][n] * W1[j][i]
+                self.z1[n][j] = np.tanh(self.a1[n][j])
+
 
             #Output layer
             y = np.zeros(self.dout)
+
             softmax_sum = 0
             for k in range(self.dout):
                 #adding offset first
-                yk = self.b2[k]
+                self.[n][k] = self.b2[k]
                 #Go thru hidden layer [0...M]
-                for j in range(z.shape[0]):
-                    # zj = z[j] activated value of hidden unit, wjk = b2[j] weight of that unit twards the output
-                    yk += z[j] * W2[k][j]
-                softmax_sum += np.exp(yk)
-                y[k] = np.exp(yk)
+                for j in range(self.hidden_units):
+                    # z[j] activated value of hidden unit, W2[k][j] weight of that unit twards the output
+                    self.a2[n][k] += z[j] * W2[k][j]
+                softmax_sum += np.exp(self.a2[n][k])
+                y[k] = np.exp(self.a2[n][k])
 
-            #Convert to softmax value
-            for k in range(self.dout):
-                y[k] = y[k]/softmax_sum
+            y = y/softmax_sum
 
             outputs[n] = y
 
         return outputs.transpose()
 
     def sgd_step(self, xdata, ydata, learn_rate):
-        ''' Do one step of SGD on xdata/ydata with given learning rate. ''' 
-
-        #Chose a random data point
-        x = np.zeros((1, xdata.shape[0]))
-        print(x)
-        x[0] = np.asarray(random.choices(xdata.transpose()))
-        y_pred = self.eval(x.transpose())
+        ''' Do one step of SGD on xdata/ydata with given learning rate. 
+            softmax outputs and cross-entropy error
+        ''' 
+        y_pred = self.eval(xdata)
         print(y_pred)
-        #del_E/del_w for each output unit
-        del_E = np.zeros(self.dout)
-        #for k in range (self.dout):
 
+        #del_E/del_w for each output unit
+        del_E = np.zeros((N, self.dout))
+        #for k in range (self.dout):
+        for wk in range(self.dout):
+            wk = 
         pass
         
     def grad(self, xdata, ydata):
@@ -110,5 +120,39 @@ class MLP:
         Note:  You should calculate this with backprop,
         but you might want to use finite differences for debugging.
         '''
-        pass
+        N = xdata.shape[1]
+        dE_dW1 = np.zeros((self.din, self.hidden_units))
+        dE_dW2 = np.zeros((self.hidden_units, self.dout))
+
+        #(N * dout) computed as yk - tk for all n
+        delta_k = (ydata - y_pred).transpose()
+        #(N * hidden_units)
+        delta_j = np.zeros((N, self.hidden_units))
+
+        #eval to get predictions and cache of a1, z1, a2
+        y_pred = self.eval(xdata)
+
+        for n in range(N):
+
+            #Output layer
+            for k in range(self.dout):
+                for j in range(self.hidden_units):
+                    # del_En/del_Wjk = delta_k * (input from j to k)
+                    dE_dW2[j][k] += delta_k[n][k] * self.z1[n][j]
+
+            #Hidden unit
+            for j in range(self.hidden_units):
+
+                # delta_j = h'(aj) * SUM_over_k(Wkj * delta_k)
+                prop_sum = 0 
+                for k in range(self.dout):
+                    prop_sum += self.W2[k][j] * delta_k[n][k]
+                delta_j[n][j] = (1 - self.a1[n][j]**2) * prop_sum
+
+                for i in range(self.din):
+                    # del_En/del_Wij = delta_j * (input from i to j)
+                    dE_dW1[i][j] += delta_j[n][j] * xdata[i][n]
+        
+        return (dE_dW1, dE_dW2)
+        
 
