d4873b1c5f6d5df7d7320bab1e7e62c67c683773
CeciliaZ030 <45245961+CeciliaZ030@users.noreply.github.com>
2020-03-10 21:58:58 -0400

    Bash to run experiments

---
+++ starterCode/Bash1.bat
@@ -0,0 +1,31 @@
+py train_mlp.py --train_file data/higgs.train --hidden_units 5 
+py train_mlp.py --train_file data/higgs.train --hidden_units 6
+py train_mlp.py --train_file data/higgs.train --hidden_units 7 
+py train_mlp.py --train_file data/higgs.train --hidden_units 8 
+py train_mlp.py --train_file data/higgs.train --hidden_units 9 
+py train_mlp.py --train_file data/higgs.train --hidden_units 10 
+py train_mlp.py --train_file data/higgs.train --hidden_units 11
+py train_mlp.py --train_file data/higgs.train --hidden_units 12 
+py train_mlp.py --train_file data/higgs.train --hidden_units 13
+py train_mlp.py --train_file data/higgs.train --hidden_units 14 
+py train_mlp.py --train_file data/higgs.train --hidden_units 15
+py train_mlp.py --train_file data/higgs.train --hidden_units 16 
+py train_mlp.py --train_file data/higgs.train --hidden_units 17
+py train_mlp.py --train_file data/higgs.train --hidden_units 18 
+py train_mlp.py --train_file data/higgs.train --hidden_units 19
+py train_mlp.py --train_file data/higgs.train --hidden_units 20
+py train_mlp.py --train_file data/higgs.train --hidden_units 21
+py train_mlp.py --train_file data/higgs.train --hidden_units 22
+py train_mlp.py --train_file data/higgs.train --hidden_units 23
+py train_mlp.py --train_file data/higgs.train --hidden_units 24
+py train_mlp.py --train_file data/higgs.train --hidden_units 25
+py train_mlp.py --train_file data/higgs.train --hidden_units 26
+py train_mlp.py --train_file data/higgs.train --hidden_units 27
+py train_mlp.py --train_file data/higgs.train --hidden_units 28
+py train_mlp.py --train_file data/higgs.train --hidden_units 29
+py train_mlp.py --train_file data/higgs.train --hidden_units 30
+
+pause
+pause
+pause
+pause
---
+++ starterCode/Bash2.bat
@@ -0,0 +1,17 @@
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.01
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.03
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.05
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.07
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.09
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.1
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.2
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.4
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.6
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.8
+py train_mlp.py --train_file data/higgs.train --learn_rate 0.1
+
+
+pause
+pause
+pause
+pause
